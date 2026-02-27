f08242e47318c0d6ed47d33ddd49312db17972bc
PanaShusco <joelllusco1@gmail.com>
2026-02-27 09:15:45 -0400

    estructura base de la pagina

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="contenedor-principal">
+      <header className="cabecera">
+        <h1>HELL GAMES</h1>
       </header>
+
+      <nav className="barra-atajos">
+        <a href="#noticias" className="btn-atajo">Noticias</a>
+        <a href="#reviews" className="btn-atajo">Reseñas</a>
+        <a href="#lanzamientos" className="btn-atajo">Salidas</a>
+        <a href="#footer" className="btn-atajo">Acerca de</a>
+      </nav>
+
+      <div className="contenido-central">
+        <main id="noticias" className="seccion-noticias">
+          <h2>Últimas Noticias</h2>
+          <p className="vacio">Vacio</p>
+        </main>
+      </div>
+
+      <footer id="footer" className="pie-pagina">
+        <p>Hell games 2026</p>
+      </footer>
     </div>
   );
 }
